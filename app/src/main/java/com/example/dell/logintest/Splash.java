18028abd80d828949ea38ebedfdd9a4feadc9794
shahidbaloc <shahidibrahim776@gmail.com>
2018-09-28 13:22:49 +0500

    pro

--- app/src/main/java/com/example/dell/logintest/Splash.java
+++ app/src/main/java/com/example/dell/logintest/Splash.java
@@ -12,14 +12,17 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        // we are using handler thread because of handling splash screem timing
+        // this run on main UI thread
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // here we are taking intent for this class(MainActivity)
                 Intent i = new Intent(Splash.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
+            // 5 second for the splash screen
         }, 5000);
 
     }
